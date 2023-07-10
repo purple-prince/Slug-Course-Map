2fae2eb7db40c427cccc9558cab9f024ed329d4b
purple-prince <himynameischuk44@gmail.com>
2023-07-10 00:51:39 -0500

    Initial Commit

---
+++ Slug Course Map/Slug_Course_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slug_Course_MapApp.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Slug_Course_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
