c0449728a421eb2bc02e28456d1701c372bce453
purple-prince <himynameischuk44@gmail.com>
2023-08-22 23:14:54 -0500

    some ui stuff and degree progress update working on

---
+++ Slug Course Map/Main/Profile/DegreeProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  DegreeProgressView.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 8/22/23.
+//
+
+import SwiftUI
+
+struct DegreeProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DegreeProgressView()
+}
---
+++ Slug Course Map/Main/Profile/ProfileView.swift
@@ -0,0 +1,87 @@
+//
+//  ProfileView.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 7/18/23.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ProfileView: View {
+    
+    @AppStorage("taken_credits") var taken_credits: Int = 0
+    @AppStorage("taking_credits") var taking_credits: Int = 0
+    @AppStorage("selected_major") var selected_major: String = "Select Major"
+    
+    @State var showDegreeProgressView: Bool = false
+    
+        
+    var body: some View {//180 req cred
+        ZStack {
+            VStack {
+                Text("Coursework")
+                    .font(Font.system(size: 48))
+                    .bold()
+                
+                VStack(alignment: .leading) {
+                    Text("**Completed Credits:** \(taken_credits) / 180")
+                        .font(.title2)
+                    
+                    Text("**Current Term Credits:** \(taking_credits)")
+                        .font(.title2)
+                        
+                    HStack { Spacer() }
+                }
+                .padding(.top, 48)
+                
+                VStack {
+                    
+                    degreeProgressButton
+                    
+//                    ZStack(alignment: .leading) {
+//                        Capsule()
+//                            .stroke(Color.red, lineWidth: 4)
+//
+//                        Rectangle()
+//                            .fill(Color.red)
+//                            .frame(width: 150)
+//                    }
+//                    .frame(width: 300, height: 50)
+//                    .clipShape(Capsule())
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .foregroundColor(.appBlue)
+    }
+}
+
+extension ProfileView {
+    var degreeProgressButton: some View {
+        VStack {
+            HStack {
+                Text("Degree Progress")
+                    .font(.title3)
+
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                .font(.system(size: 16, weight: .semibold))
+            }
+            .padding(.horizontal)
+            .foregroundColor(.appPrimary)
+        }
+        .padding(.vertical)
+        .background(Color.white)
+        .cornerRadius(8)
+        .frame(maxWidth: .infinity)
+        .shadow(color: Color(red: 0.7, green: 0.7, blue: 0.7), radius: 10)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
