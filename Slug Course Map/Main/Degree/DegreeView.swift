080162234ea6253ae6dad1b39efbb41ddb4253b5
purple-prince <himynameischuk44@gmail.com>
2023-09-08 02:12:31 -0500

    redoing the degree view and added a bunch of campus map stuff

---
+++ Slug Course Map/Main/Degree/DegreeView.swift
@@ -0,0 +1,18 @@
+//
+//  DegreeView.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 9/7/23.
+//
+
+import SwiftUI
+
+struct DegreeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DegreeView()
+}
---
+++ Slug Course Map/Main/Map/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 9/6/23.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
