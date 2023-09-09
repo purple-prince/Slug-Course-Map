609f158c5a17332eb536ef859111b46e9c9bcc31
purple-prince <himynameischuk44@gmail.com>
2023-09-08 21:26:35 -0500

    too tired for commit message

---
+++ Slug Course Map/Main/Degree/DegreeDetail.swift
@@ -0,0 +1,18 @@
+//
+//  DegreeDetail.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 9/8/23.
+//
+
+import SwiftUI
+
+struct DegreeDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DegreeDetail()
+}
