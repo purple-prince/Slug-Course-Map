0f3e731d3fdf04ddfb31576e59fccb89078cbdfb
purple-prince <himynameischuk44@gmail.com>
2023-09-15 23:52:26 -0500

    refactoring pt sintax no arch

---
+++ Slug Course Map/Files/HelperViews/BackButton.swift
@@ -0,0 +1,18 @@
+//
+//  BackButton.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 9/15/23.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButton()
+}
