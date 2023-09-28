c49c6d213a0a11910d11bcb989e9567576c94d2f
purple-prince <himynameischuk44@gmail.com>
2023-09-27 18:57:49 -0700

    options area idk

---
+++ Slug Course Map/Main/Options/OptionsView.swift
@@ -0,0 +1,18 @@
+//
+//  OptionsView.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 9/27/23.
+//
+
+import SwiftUI
+
+struct OptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionsView()
+}
