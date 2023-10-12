eb2eea65cc3c087e98cea95c2f716ed539b1b977
purple-prince <himynameischuk44@gmail.com>
2023-10-11 17:04:44 -0700

    little animation for when app initializes

---
+++ Slug Course Map/Files/HelperViews/SquiggleShape.swift
@@ -0,0 +1,18 @@
+//
+//  SquiggleShape.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 10/11/23.
+//
+
+import SwiftUI
+
+struct SquiggleShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SquiggleShape()
+}
