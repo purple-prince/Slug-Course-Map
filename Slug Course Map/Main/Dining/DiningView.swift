bb756bb254956ef590407f08b6fdf612c07a7871
purple-prince <himynameischuk44@gmail.com>
2023-10-16 22:48:51 -0700

    lots of updates: dining info setup, ui changes

---
+++ Slug Course Map/Main/Dining/DiningView.swift
@@ -0,0 +1,18 @@
+//
+//  DiningView.swift
+//  Slug Course Map
+//
+//  Created by Charlie Reeder on 10/16/23.
+//
+
+import SwiftUI
+
+struct DiningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiningView()
+}
